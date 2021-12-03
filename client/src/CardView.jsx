5db6c78a9b4fa2356aee0758be89e5095c5922e1
Derek Mason <3derekmason@gmail.com>
2021-12-03 11:22:34 -0700

    AppContext init

--- client/src/CardView.jsx
+++ client/src/CardView.jsx
@@ -4,13 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faMicroscope, faSeedling } from '@fortawesome/free-solid-svg-icons'
 
 const CardView = () => {
-
   return (
       <Container id="cardview">
         <div className="mui--text-caption">Cards for each db Activity will be displayed here</div>
         <Card>
-          <div className="mui--text-title">Activity Name</div>
-          <div className="mui--text-body2">Activity description here.</div>
+          <div className="mui--text-title">{activities}</div>
         </Card>
       </Container>
   );
