a7432073bb83fcb91f4f1ad3bb0207a3ad950024
Derek Mason <3derekmason@gmail.com>
2021-12-03 10:55:26 -0700

    render card and to app ActForm component

---
+++ client/src/ActForm.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { Card, CardContent } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faMicroscope, faSeedling } from '@fortawesome/free-solid-svg-icons'
+
+const ActForm = () => {
+
+  return (
+    <Card>
+      <CardContent>
+        <div className="mui--text-display4">Filter/Add Activity Form</div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ActForm;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,18 @@ import Appbar from 'muicss/lib/react/appbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMicroscope, faSeedling } from '@fortawesome/free-solid-svg-icons'
 
+import ActForm from './ActForm.jsx';
+
 const App = () => {
 
   return (
+    <div>
       <Appbar>
         <div className="mui--text-display2">steMVP <FontAwesomeIcon icon={faSeedling} /></div>
         <div className="mui--text-subhead"><FontAwesomeIcon icon={faMicroscope} style={{marginRight: "8px"}}/>About</div>
       </Appbar>
+      <ActForm />
+    </div>
   );
 };
 
