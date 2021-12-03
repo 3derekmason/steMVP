f34ad9c3ec79fb6ecd79668364385d9ab2cc1339
Derek Mason <3derekmason@gmail.com>
2021-12-03 08:01:24 -0700

    create material ui primary and secondary colory theme in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
 import App from './app.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const theme = createMuiTheme({
+  palette: {
+     primary: {
+        main: "#00BCD4" // This is an cyan color
+               },
+     secondary: {
+        main: "#18FFFF" //Another brighter cyan color
+                }
+           }
+});
+
+ReactDOM.render(<ThemeProvider theme={theme}><App /></ThemeProvider>, document.getElementById('app'));
