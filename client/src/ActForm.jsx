95c7f9db6032f6037e46b7976ffd267f3322c9af
Derek Mason <3derekmason@gmail.com>
2021-12-03 16:13:06 -0700

    form and buttons setup

--- client/src/ActForm.jsx
+++ client/src/ActForm.jsx
@@ -1,15 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent } from '@material-ui/core';
+import { Button, Card, CardContent, FormControl, FormHelperText, InputLabel, Select, NativeSelect } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faMicroscope, faSeedling } from '@fortawesome/free-solid-svg-icons'
 
+
 const ActForm = () => {
+
+  const [currentCategory, setCurrentCategory] = useState();
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  };
+
 
   return (
     <Card id="actform">
-      <CardContent>
-        <div className="mui--text-display4">Filter/Add Activity Form</div>
-      </CardContent>
+      <div className="filter" style={{width: '66%'}}>
+      <FormControl >
+        <InputLabel htmlFor="category-native-helper">Category</InputLabel>
+        <NativeSelect
+          value={''}
+          onChange={handleChange}
+          inputProps={{
+            name: 'category',
+            id: 'category-native-helper',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option >Art</option>
+          <option >Bugs</option>
+          <option >Dinosaurs</option>
+          <option >Music</option>
+          <option >Nature</option>
+          <option >Ocean</option>
+          <option >Sensory</option>
+          <option >Space</option>
+        </NativeSelect>
+        <FormHelperText>Filter by category</FormHelperText>
+      </FormControl>
+      </div>
+      <div className="buttons" style={{width: '33%'}}>
+        <Button variant="contianed" color="primary">Add Activity</Button>
+        <Button color="secondary">RANDOM ACTIVITY</Button>
+      </div>
     </Card>
   );
 };
